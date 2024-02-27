de56029489533a72a1cb48cb64c57f35d31d732b
omurbekavalbekov <omurbekavalbekov9@gmail.com>
2024-02-27 14:41:13 +0800

    commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int katet1 = 13;
+        int katet2 = 15;
+        double gipotenuza = Math.sqrt((katet1 * katet1) + (katet2 * katet2));
+        System.out.println("Gipotenuza treugolnika  = " + gipotenuza);
+    }
+}
